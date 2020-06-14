325f2ec2919be2076b5ac0e41f6542d04f53f7bc
Sandeep <san_shri05@yahoo.co.in>
2020-06-15 05:14:51 +0530

    Initial Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Sandeep
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		for (int i=1;i<=10;i++)
+		System.out.println("Hello World");
+		// TODO Auto-generated method stub
+	}
+
+}
